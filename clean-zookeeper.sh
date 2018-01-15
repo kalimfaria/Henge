3c526247dcd0032a27d143df9c3df2304ae05bf5
Faria Kalim <fariakalim@Farias-MacBook-Pro.local>
2018-01-16 00:48:47 +0500

    removing scripts

--- clean-zookeeper.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-sudo killall java
-cd /var/zookeeper
-sudo rm -r *
-cd /tmp
-sudo rm -r *.log
-cd /var/nimbus/storm/logs
-sudo rm -r *
--- fabfile.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-from fabric.api import env, run
-env.user = <your user name>
-env.password = <your password>
-env.hosts = [ 'node1', 'node2', 'node3', 'node4', 'node5' ]
-def delete():
-    run("cd /var/latencies && sudo rm -r *")
-
